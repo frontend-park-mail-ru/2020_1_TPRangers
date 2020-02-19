f621b5d2c6044558a7f7dbddeeab27e60e0b1400
GuVictory <gubanova.vi10@gmail.com>
2020-02-19 18:57:23 +0300

    F-22: added template for mainPage

--- public/modules/createMainPage.js
+++ public/modules/createMainPage.js
@@ -1,32 +1,56 @@
-import createLinks from "./createLinks.js";
-
-const dataForMainPage = {
-  login: {
-    name: 'Вход',
-    link: 'login',
-    cl: 'main_link',
-  },
-  registration: {
-    name: 'Регистрация',
-    link: 'registration',
-    cl: 'main_link',
-  },
-  settings: {
-    name: 'Настройки',
-    link: 'settings',
-    cl: 'main_link',
-  },
-  about: {
-    name: 'О проекте',
-    link: 'about',
-    cl: 'main_link',
-  },
-};
+let menuTemplate = ' <% items.forEach(function(item) { %> \
+                          <a class="<%=item.cl%>" href="/<%=item.link%>" data-section="<%=item.link%>"><%-item.name%></a> \
+                          <% }); %>';
 
 export function createMainPage(parent) {
-  parent.innerHTML = '';
-  Object.values(dataForMainPage).forEach(function (key, val) {
-    parent.appendChild(createLinks(key));
+  let menu = new Menu({
+    // передаём также шаблоны
+    template: _.template(menuTemplate),
+    items: [
+      {
+        name: 'Вход',
+        link: 'login',
+        cl: 'main_link',
+      },
+      {
+        name: 'Регистрация',
+        link: 'registration',
+        cl: 'main_link',
+      },
+      {
+        name: 'Настройки',
+        link: 'settings',
+        cl: 'main_link',
+      },
+      {
+        name: 'О проекте',
+        link: 'about',
+        cl: 'main_link',
+      },
+    ]
   });
-
+  parent.appendChild(menu.getElem());
 }
+
+function Menu(options) {
+  let elem;
+
+  function getElem() {
+    if (!elem) render();
+    return elem;
+  }
+
+  function render() {
+    let html = options.template({
+      items: options.items
+    });
+
+    elem = document.createElement('div');
+    elem.classList.add('mainMenu');
+    elem.innerHTML = html;
+  }
+
+
+  this.getElem = getElem;
+}
+
