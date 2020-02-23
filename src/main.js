dc49595cfbd321fbdb36c3554d74e41217ac87bb
alexey ershkov <ershkov.alex2000@gmail.com>
2020-02-23 21:50:48 +0300

    fixed issue with anchors in  SPA

--- src/main.js
+++ src/main.js
@@ -4,16 +4,16 @@ import {createRegistration} from "./modules/createRegistration";
 import {createSettings} from "./modules/createSettings";
 import  "./css/styles.css"
 import  "./css/normalize.css"
+import {createProfile} from "./modules/createProfile";
 
 
 const app = document.getElementById("application");
 
 app.addEventListener('click', function (evt) {
   const {target} = evt;
-  console.log(evt)
   if (target instanceof HTMLAnchorElement) {
     evt.preventDefault();
-    console.log(target.pathname);
+    routes[target.getAttribute('section')](app);
   }
 });
 
@@ -25,7 +25,10 @@ app.addEventListener('load', (event) => {
 
 const routes = {
   main: createMainPage,
-  login: createLogin
+  login: createLogin,
+  registration: createRegistration,
+  settings: createSettings,
+  // about: createProfile,
 };
 
 createMainPage(app);
