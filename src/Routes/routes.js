42953e8b1b037feed02877fc40797acdb1912665
GuVictory <gubanova.vi10@gmail.com>
2020-03-12 00:13:19 +0300

    router  and refactor

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,47 +1,22 @@
-/**
- * модуль хранящий в себе роутинг проекта
- * @module routes
- */
-
-/**
- * @description Настройка роутов для проекта, вызов различных функций для разных рутов
- * @const {
- *   routes — хранит список зарегистрированных точек
- *   mode — может принимать значение hash или history в зависимости от поддержки History API
- *   root — корневая точка приложения, необходима только при использовании pushState
- * }
- */
-// eslint-disable-next-line import/prefer-default-export
 export const Router = {
   routes: [],
   mode: null,
   root: '/',
 
-  /**
-   * @description Метод для первичной настройки принимает
-   * два параметра, можно передать их в одном объекте.
-   * @param options  Режим ‘history’ будет включен только в том случае если передать
-   * необходимый параметр и при условии что браузер поддерживает pushState.
-   */
   config(options) {
-    this.mode =
-      options && options.mode && options.mode == 'history' && !!history.pushState
+    this.mode =      options && options.mode && options.mode == 'history' && !!history.pushState
         ? 'history'
         : 'hash';
     this.root = options && options.root ? `/${this.clearSlashes(options.root)}/` : '/';
     return this;
   },
 
-  /**
-   * @description Метод отвечает за определение текущего адреса.
-   * Так как роутер работает в одном из двух режимов, то и обрабатывать url будем по разному.
-   */
   getFragment() {
     let fragment = '';
     if (this.mode === 'history') {
       fragment = this.clearSlashes(decodeURI(location.pathname + location.search));
       fragment = fragment.replace(/\?(.*)$/, '');
-      fragment = this.root !== '/' ? fragment.replace(this.root, '') : fragment;
+      fragment = this.root != '/' ? fragment.replace(this.root, '') : fragment;
     } else {
       const match = window.location.href.match(/#(.*)$/);
       fragment = match ? match[1] : '';
@@ -49,9 +24,6 @@ export const Router = {
     return this.clearSlashes(fragment);
   },
 
-  /**
-   * @description удаляет слэши в начале и конце, для того чтобы унифицировать адрес
-   */
   clearSlashes(path) {
     return path
       .toString()
@@ -59,28 +31,15 @@ export const Router = {
       .replace(/^\//, '');
   },
 
-  /**
-   * @description Метод add наполняет массив маршрутов, путём передачи параметров: путь, обработчик
-   *  то считать эту функцию (колбэк) обработчиком корневого маршрута.
-   * @param re - путь
-   * @param handler - обработчик
-   *  В случае если на вход add дан только один параметр и этот параметр функция,
-   *  то считать эту функцию (колбэк) обработчиком корневого маршрута.
-   */
   add(re, handler) {
     if (typeof re === 'function') {
       handler = re;
-      re = '';
+      re = '/';
     }
     this.routes.push({ re, handler });
-    console.log(this.routes);
     return this;
   },
 
-  /**
-   * @description Удаление маршрута произойдёт только в том случае, если передать
-   * точно такое же регулярное выражение или функцию обрабочик.
-   */
   remove(param) {
     for (let i = 0, r; i < this.routes.length, (r = this.routes[i]); i++) {
       if (r.handler === param || r.re.toString() === param.toString()) {
@@ -91,9 +50,6 @@ export const Router = {
     return this;
   },
 
-  /**
-   * @description сбрасывает список маршрутов и режим работы.
-   */
   flush() {
     this.routes = [];
     this.mode = null;
@@ -101,15 +57,11 @@ export const Router = {
     return this;
   },
 
-  /**
-   * @description  метод для сравнения текущего адреса и зарегистрированных маршрутов.
-   * @param f - в качестве параметра получает url, если параметр пуст, то при помощи getFragment
-   */
   check(f) {
     const fragment = f || this.getFragment();
     for (let i = 0; i < this.routes.length; i++) {
-      const match = fragment.match(this.routes[i].re) || [];
-      if (match.length !== 0 && match[0].startsWith(fragment[0])) {
+      const match = fragment.match(this.routes[i].re);
+      if (match) {
         match.shift();
         this.routes[i].handler.apply({}, match);
         return this;
@@ -118,10 +70,6 @@ export const Router = {
     return this;
   },
 
-  /**
-   * @description триггер изменений в адресной строке.
-   * Храним последний активный url, чтобы сравнить с новым.
-   */
   listen() {
     const self = this;
     let current = self.getFragment();
@@ -136,9 +84,6 @@ export const Router = {
     return this;
   },
 
-  /**
-   * @description возможности самостоятельно менять адрес
-   */
   navigate(path) {
     path = path || '';
     if (this.mode == 'history') {
@@ -147,5 +92,22 @@ export const Router = {
       window.location.href = `${window.location.href.replace(/#(.*)$/, '')}#${path}`;
     }
     return this;
+  },
+
+  callCurrent() {
+    const fragment = this.getFragment();
+    console.log(fragment);
+    const result = this.routes.find((item, index) => {
+      if (fragment.match(item.re)) {
+        console.log(item.re);
+        return true;
+      }
+      return false;
+    });
+    if (result) {
+      result.handler();
+      return true;
+    }
+    return false;
   },
 };
---
+++ src/modules/index.js
@@ -0,0 +1,71 @@
+// Main modules file
+import { Router } from '../Routes/routes';
+
+const leftBlockTmpl = require('../pug/includes/modules/left-block.pug');
+const testTmpl = require('../pug/pages/news.pug');
+
+const app = document.getElementById('application');
+if (!app) console.log('app not found');
+
+app.addEventListener('click', evt => {
+  if (evt.target instanceof Element) {
+    if (evt.target.tagName === 'IMG') {
+      evt.preventDefault();
+
+      const aNode = evt.target.parentNode;
+
+      if (aNode.getAttribute('section') === 'profile') {
+        // TODO: Тут надо будет добавить обработку id  пользователя, чья страница
+        Router.navigate(`${aNode.getAttribute('section')}/2`);
+      } else {
+        Router.navigate(aNode.getAttribute('section'));
+      }
+    } else if (evt.target.tagName === 'A') {
+      evt.preventDefault();
+      Router.navigate(evt.target.getAttribute('section'));
+    }
+  }
+});
+
+const leftBlock = document.getElementById('left-block');
+leftBlock.innerHTML += leftBlockTmpl();
+
+const mainBlock = document.getElementById('main-block');
+
+// const rightBlock = document.getElementById('right-block');
+
+// TODO: Тут надо добавить обработчики всех страниц
+Router.config({ mode: 'history' });
+
+Router.add(/news/, () => {
+  console.log('news');
+  mainBlock.innerHTML = testTmpl({ data: 'Новости' });
+})
+  .add(/friends/, () => {
+    console.log('friends');
+    mainBlock.innerHTML = testTmpl({ data: 'Друзья' });
+  })
+  .add(/messages/, () => {
+    console.log('messages');
+    mainBlock.innerHTML = testTmpl({ data: 'Сообщения' });
+  })
+  .add(/media/, () => {
+    console.log('media');
+    mainBlock.innerHTML = testTmpl({ data: 'Мадиатека' });
+  })
+  .add(/settings/, () => {
+    console.log('settings');
+    mainBlock.innerHTML = testTmpl({ data: 'Настройки' });
+  })
+  .add(/profile\/(.*)/, () => {
+    console.log(Router.getFragment());
+    console.log('profile');
+    mainBlock.innerHTML = testTmpl({ data: 'Профиль пользователя' });
+  })
+  .add(/main/, () => {
+    console.log('main');
+    mainBlock.innerHTML = testTmpl({ data: 'Главная страница' });
+  })
+  .listen();
+
+Router.callCurrent();
