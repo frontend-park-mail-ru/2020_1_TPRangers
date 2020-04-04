179c7659120f0741d3d08374f91732b6425c07c7
alexey ershkov <ershkov.alex2000@gmail.com>
2020-04-04 20:25:18 +0300

    added cors

--- photo_server/index.js
+++ photo_server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = 5000;
 const router = require('./router');
 
+app.use(cors());
 app.use('/', router);
 
 app.listen(port, (err) => {
