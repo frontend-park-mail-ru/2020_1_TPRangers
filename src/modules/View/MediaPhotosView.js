57ec53fed44595e6d8634a5444154d9f10891339
alexey ershkov <ershkov.alex2000@gmail.com>
2020-04-08 18:51:59 +0300

    save

--- src/modules/View/MediaPhotosView.js
+++ src/modules/View/MediaPhotosView.js
@@ -1,6 +1,7 @@
 import IView from './IView';
 import Observer from '../../controller/observer';
 import { fetchGET } from '../../ajax/ajax';
+import {Router} from '../../Routes/routes';
 
 
 const photosTmpl = require('../../pug/pages/mediaPhotos.pug');
@@ -9,9 +10,9 @@ export default class MediaPhotosView extends IView{
 
     render() {
       super.clear();
-
+      const albumUrl = Router.getFragment().split('/')[1];
       fetchGET({
-        url: BACKEND_IP + '/api/v1/friends',
+        url: BACKEND_IP + '/api/v1/albums/' + albumUrl,
 
         callback: response => {
           response.json().then(response => {
