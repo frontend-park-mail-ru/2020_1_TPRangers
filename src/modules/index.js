86b18105e120653342f753e7ce653ec656babb9f
alexey ershkov <ershkov.alex2000@gmail.com>
2020-04-19 22:17:09 +0300

    ws started

--- src/modules/index.js
+++ src/modules/index.js
@@ -64,6 +64,31 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+// ws register
+
+let socket = new WebSocket(`ws://localhost:3001`);
+
+socket.onopen = function(e) {
+  console.log("[open] Соединение установлено");
+};
+
+socket.onmessage = function(event) {
+  console.log(`[message] Данные получены с сервера: ${event.data}`);
+};
+
+socket.onclose = function(event) {
+  if (event.wasClean) {
+    console.log(`[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
+  } else {
+    // например, сервер убил процесс или сеть недоступна
+    // обычно в этом случае event.code 1006
+    console.log('[close] Соединение прервано');
+  }
+};
+
+socket.onerror = function(error) {
+  console.log(`[error] ${error.message}`);
+};
 
 
 
